0ebc38b22e4376cf88181e55d75d5353ae607428
José Antonio Solórzano Flores <98479861+Tono84@users.noreply.github.com>
2024-07-22 16:06:31 -0600

    HU16

---
+++ GanaderiaLasDelicias/Models/Severance.cs
@@ -0,0 +1,14 @@
+﻿namespace GanaderiaLasDelicias.Models
+{
+    public class Severance
+    {
+        public decimal FinalSalary {get; set;}
+
+        public int YearsWorked {get; set;}
+
+        public bool GaveNotice { get; set;} 
+
+
+        public decimal Cesantia { get; set; }
+    }
+}
