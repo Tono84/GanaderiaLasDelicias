eeeb96362546c2f8225ee010e3b376b1bd74dd85
José Antonio Solórzano Flores <98479861+Tono84@users.noreply.github.com>
2024-10-19 16:54:18 -0600

    TCID 11 comments fixed

--- GanaderiaLasDelicias/Models/Schedule.cs
+++ GanaderiaLasDelicias/Models/Schedule.cs
@@ -10,26 +10,30 @@ namespace GanaderiaLasDelicias.Models
 
 
         [Display(Name="Nombre")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "El nombre solo debe contener letras.")]
         [Required(ErrorMessage = "El campo Nombre es requerido.")]
         [StringLength(100)]
         public string Name { get; set; }
 
         [Display(Name = "Turno")]
+
         [Required(ErrorMessage = "El campo Tipo de Turno es requerido.")]
-        public int ShiftType { get; set; } // 1 para diurno, 2 para nocturno
+        public int ShiftType { get; set; }
         [Display(Name = "Días Laborales")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Los días laborales solo deben contener letras.")]
         [Required(ErrorMessage = "El campo Días a Trabajar es requerido.")]
         [StringLength(50)]
         public string WorkDays { get; set; }
 
         [Display(Name = "Días Libres")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Los días de descanso solo deben contener letras.")]
         [Required(ErrorMessage = "El campo Días Libres es requerido.")]
         [StringLength(50)]
         public string OffDays { get; set; }
 
         [Display(Name = "Horas de Trabajo")]
         [Required(ErrorMessage = "El campo Horas de Trabajo es requerido.")]
-        [StringLength(50)]
+        [StringLength(15)]
         public string WorkHours { get; set; }
 
         [Display(Name = "Estado")]
