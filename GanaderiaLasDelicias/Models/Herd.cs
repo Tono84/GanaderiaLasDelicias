4d68a6ce055bd66c7b4c53ec73ea9d566649e3f2
José Antonio Solórzano Flores <98479861+Tono84@users.noreply.github.com>
2024-11-03 17:00:17 -0600

    se agrega validaciones en herd

--- GanaderiaLasDelicias/Models/Herd.cs
+++ GanaderiaLasDelicias/Models/Herd.cs
@@ -21,18 +21,33 @@ namespace GanaderiaLasDelicias.Models
         [Key]
         public int CowId { get; set; }
 
+        [StringLength(15)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "El nombre solo debe contener letras.")]
+        [Required(ErrorMessage = "El nombre es requerido")]
         [Display(Name="Nombre")]
         public string Name { get; set; } = null!;
 
+        [RegularExpression(@"^\d+(\.\d{1})?$", ErrorMessage = "El número asignado debe de ser un número entero.")]
+        [Range(0, 999, ErrorMessage = "El número asignado debe estar entre 0 y 999")]
+        [Required(ErrorMessage = "El número asignado es requerido")]
         [Display(Name = "Número Asignado")]
         public int Number { get; set; }
 
+        [StringLength(15)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "La raza solo debe contener letras.")]
+        [Required(ErrorMessage = "La raza es requerida")]
         [Display(Name = "Raza")]
         public string Race { get; set; } = null!;
 
-        [Display(Name = "Edad (Años)")]
+        [RegularExpression(@"^\d+(\.\d{1})?$", ErrorMessage = "La edad debe de ser un número entero.")]
+        [Range(0, 999, ErrorMessage = "La edad debe estar entre 0 y 999")]
+        [Required(ErrorMessage = "La edad es requerida")]
+        [Display(Name = "Edad(años)")]
         public int Age { get; set; }
 
+        [RegularExpression(@"^\d+(\.\d{1,2})?$", ErrorMessage = "El peso debe ser un número decimal con hasta dos decimales.")]
+        [Range(0, 999.99, ErrorMessage = "El peso debe estar entre 0 y 999.99")]
+        [Required(ErrorMessage = "El Peso es requerido")]
         [Display(Name = "Peso")]
         public decimal Weight { get; set; }
 
